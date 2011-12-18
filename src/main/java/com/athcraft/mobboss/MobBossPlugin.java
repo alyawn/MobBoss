bbc78ff12651605fe13ac563f25edf5d79a4b5ac
Alan Moss <alyawn@gmail.com>
2011-12-17 23:01:05 -0500

    Added initial shell and dependancy on bukkit.

---
+++ src/main/java/com/athcraft/mobboss/MobBossPlugin.java
@@ -0,0 +1,18 @@
+/**
+ * MobBoss Plugin used to control mob spawning on bukkit server.
+ * Originally developed for Athcraft. http://athcraft.com
+ *
+ * @author Alan Moss <alyawn@gmail.com>
+ */
+package com.athcraft.mobboss;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobBossPlugin extends JavaPlugin {
+
+    public void onEnable() {}
+    public void onDisable() {}
+
+
+}
+
